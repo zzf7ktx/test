237e671c353148cc4ca1772190df1b6421737786
zzf7ktx <zzf7ktx@gmail.com>
2019-10-31 00:37:57 +0700

    add file

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+ // Listen on port 5000
+app.listen(port, () => {
+   console.log(`Server is booming on port 5000
+   Visit http://localhost:5000`);
+});
+app.get("/", (req, res) => {
+      res.send("Welcome to a basic express App");
+});
+app.get("/users", (req, res) => {
+      res.json([
+	      { name: "William", location: "Abu Dhabi" },
+	      { name: "Chris", location: "Vegas" }
+	    ]);
+});
+
+app.post("/user", (req, res) => {
+      const { name, location } = req.body;
+
+      res.send({ status: "User created", name, location });
+});
